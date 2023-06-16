6677f0c77a80d80f45841b5bfd81a33a4fd4124d
Nat66615 <nat.ja@mail.ru>
2023-06-16 15:14:31 +0500

    tag include

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='horoscope_index'),
     path('<int:zodiac_sign>/', views.get_zodiac_sign_by_number),
-    path('<str:zodiac_sign>/', views.get_zodiac_sign, name=('horoscope_name')),
-    path('type', views.type, name=('element_name')),
+    path('<str:zodiac_sign>/', views.get_zodiac_sign, name='horoscope_name'),
+    path('type', views.type, name='element_name'),
     path('type/<one_element>', views.get_sign_for_element)
 ]
